6fb085bc9273ee2159dbb1fb27d11d9e0b9f519b
Umut Ugur <umutugur@outlook.com.tr>
2025-07-02 14:16:41 +0300

    Full backend push

--- backend/routes/notification.js
+++
@@ -1,9 +0,0 @@
-// backend/routes/notification.js
-const express = require('express');
-const router = express.Router();
-const { sendPushNotification } = require('../controllers/notificationController');
-
-// 🔐 Bildirim gönderme (admin kullanımı)
-router.post('/send', sendPushNotification);
-
-module.exports = router;
