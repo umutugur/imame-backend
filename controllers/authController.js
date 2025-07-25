61822144d28bec3b5109e5e5920f94afa3bb5108
Umut Ugur <umutugur@outlook.com.tr>
2025-07-25 14:51:42 +0300

    Bug fix

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,7 +1,7 @@
 const User = require('../models/User');
 const bcrypt = require('bcryptjs');
 const axios = require('axios');
-const { admin } = require('../utils/firebaseAdmin');
+
 
 // Kullanıcı Kaydı
 exports.register = async (req, res) => {
