b92f88b429e83b0d876ec1a8a29017e41028e845
Umut Ugur <umutugur@outlook.com.tr>
2025-07-26 15:09:49 +0300

    Rate,Report,Favorite fixes

--- models/Rating.js
+++ models/Rating.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const ratingSchema = new mongoose.Schema({
   seller: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
   buyer: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
-  auction: { type: mongoose.Schema.Types.ObjectId, ref: 'Auction', required: true },
+  auction: { type: mongoose.Schema.Types.ObjectId, ref: 'Auction' },
   score: { type: Number, min: 1, max: 5, required: true },
   comment: { type: String }
 }, { timestamps: true });
