fe0f96d74e949420e2799d4399814956bdcca257
umutugur <44439099+umutugur@users.noreply.github.com>
2025-07-27 21:12:41 +0300

    Update Message.js

--- models/Message.js
+++ models/Message.js
@@ -4,6 +4,7 @@ const messageSchema = new mongoose.Schema({
   chat: { type: mongoose.Schema.Types.ObjectId, ref: 'Chat', required: true },
   sender: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
   text: { type: String, required: true },
+  isRead: { type: Boolean, default: false }, // ✅ Yeni alan
 }, { timestamps: true });
 
 module.exports = mongoose.model('Message', messageSchema);
